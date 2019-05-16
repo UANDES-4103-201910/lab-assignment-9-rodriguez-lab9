d3191079a6f04abb96e8db05493845746e8ec949
juanesgh <jerodriguez@miuandes.cl>
2019-05-16 11:09:13 -0400

    Devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_for :users
   resources :users
-
+  
   root :to => 'sessions#new'
 
   post '/login',   to: 'sessions#create', as: :log_in
--- db/migrate/20180401225453_create_users.rb
+++ db/migrate/20180401225453_create_users.rb
@@ -3,7 +3,6 @@ class CreateUsers < ActiveRecord::Migration[5.1]
     create_table :users do |t|
       t.string :name
       t.string :last_name
-      t.string :email
       t.string :phone
       t.string :password
       t.string :address
